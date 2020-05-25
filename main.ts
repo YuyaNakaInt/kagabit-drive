a77ac651d324b3791cab8616dc3c2c929170352d
YuyaNakaInt <61772979+YuyaNakaInt@users.noreply.github.com>
2020-05-26 07:16:07 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,20 +1,28 @@
 //% weight=30 icon="\uf1b9" color=#000000 block="KAGA:bit-Drive"
 namespace kagabitdrive {
 
+    export enum ledmodeval{
+        //% block="使う"
+        Use = 1,
+        //% block="使わない"
+        NoUse = 0
+    }
     export enum direction {
         //% block="前"
         Forward = 1,
         //% block="後"
         Back = 0
     }
+    //% blockId=use_led block="LEDの利用 %ledmode %v"
+    export function useled(ledmode:ledmodeval):void{
+        if(ledmode == 1){
+            led.enable(true)
+        }
+        else if (ledmode == 1){
+            led.enable(false)
+        }else{
 
-    //% blockId=stop_led block="LEDを無効にする %v"
-    export function stopLed() {
-        led.enable(false)
-    }
-    //% blockId=start_led block="LEDを有効ににする %v"
-    export function startLed() {
-        led.enable(true)
+        }
     }
     //% blockId=show_strings block="コメント文 |%text %power %v"
     export function showcomment(text: direction,power:number): void {
