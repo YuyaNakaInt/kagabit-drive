b37eef90f48e9bb869da542bfe5eb10fa8087342
YuyaNakaInt <61772979+YuyaNakaInt@users.noreply.github.com>
2020-05-26 07:31:28 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -24,9 +24,6 @@ namespace kagabitdrive {
 
         }
     }
-    //% blockId=show_strings block="コメント文 |%text %power %v"
-    export function showcomment(text: direction,power:number): void {
-    }
 
     //% blockId=L_DCmotorAnalog
     //% block="Lモーター 方向%mode アナログ出力%power"
@@ -36,6 +33,16 @@ namespace kagabitdrive {
         pins.digitalWritePin(DigitalPin.P15, mode)
         pins.analogWritePin(AnalogPin.P16, power)
   
+    }
+
+    //% blockId=R_DCmotorAnalog
+    //% block="Rモーター 方向%mode アナログ出力%power"
+    //% power.min=0 power.max=1023
+    export function RmotorA(mode: direction, power: number) {
+
+        pins.digitalWritePin(DigitalPin.P13, mode)
+        pins.analogWritePin(AnalogPin.P14, power)
+
     }
 
 }
